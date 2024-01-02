44be9cb771d41f4e843795169aaedb951d25caeb
mahendra9408 <mahendrak91608@gmail.com>
2024-01-02 22:50:18 +0530

    updating cdk code

--- newasg/app.py
+++ newasg/app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from customasg.customasg_stack import CustomasgStack
+from newasg.newasg_stack import NewasgStack
 
 
 app = cdk.App()
-CustomasgStack(app, "CustomasgStack",
+NewasgStack(app, "NewasgStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -15,7 +15,7 @@ CustomasgStack(app, "CustomasgStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
